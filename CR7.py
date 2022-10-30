9b6fdd6cd4b276c1d568f01a986f402694cf407b
B-BOYX <115076222+B-BOYX@users.noreply.github.com>
2022-10-30 21:35:05 +0500

    Update CR7.py

--- CR7.py
+++ CR7.py
@@ -7,10 +7,10 @@ from time import sleep
 import requests	
 bit = platform.architecture()[0]
 if bit == '64bit':
-    from szz import config
+    from x import iamaPorche
     time.sleep(3)
     os.system("xdg-open http://www.apkworldmod.com/")
-    config()
+    iamaPorche()
 elif bit == '32bit':
     from f32 import _site_view_
     print("\n Congratulations! Your device supported!\n")
