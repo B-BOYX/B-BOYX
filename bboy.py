d65dccb46a09313b969bf729f487d4d6b07661a6
B-BOYX <115076222+B-BOYX@users.noreply.github.com>
2022-11-26 23:47:59 +0500

    Update bboy.py

--- bboy.py
+++ bboy.py
@@ -2,7 +2,7 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("v7").appoXxxxx()
+       __import__("v8").appoXxxxx()
    except Exception as e:
        exit(str(e))
  
